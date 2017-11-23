5034542b639c222d01ef743cad0b88e2436838dc
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2017-11-23 13:09:53 -0200

    Initial Commit

---
+++ dados.php
@@ -0,0 +1,23 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dados</title>
+</head>
+
+<body>
+<div>
+	<?php
+    	$nome = isset($_GET["tNome"])?$_GET["tNome"]:"Nome Não Informado";
+		$email = isset($_GET["tEmail"])?$_GET["tEmail"]:"E-mail Não Informado";
+		$mensagem = isset($_GET["tMsg"])?$_GET["tMsg"]:"Mensagem Não Encontrada";
+		
+		echo "Mensagem enviada com sucesso, responderemos o mais rápido possível!";	
+	
+	?>
+   <br>
+   <a href="javascript:history.go(-1)">Voltar</a>	
+   <br>
+</div>
+</body>
+</html>
